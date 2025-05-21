2c6b3c262796969135eb0fd6061f4e77bc0228c4
antoniodmota <antoniodmota1996@gmail.com>
2025-05-21 13:22:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+  const hamburguesa = document.querySelector("#hamburguer");
+  const enlaces = document.querySelector("#secondnav");
+
+  hamburguesa.addEventListener('click', ()=>{
+  enlaces.classList.toggle('show')})
+
+  enlaces.addEventListener('click', ()=>{
+           enlaces.classList.toggle('show')})
